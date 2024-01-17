c37673ab31690c2d38d960efb4f49a47d5757502
jwanp <joohwan726@naver.com>
2024-01-17 11:16:38 +0900

    [Feat] 멋있게 컴포넌트 전환 애니메이션 주는법  detail.js

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,6 +1,8 @@
-import { Outlet, useParams  } from 'react-router-dom';
-import {useState,useEffect} from 'react'; 
-import "./detail.css"
+import { Outlet, useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Nav } from 'react-bootstrap';
+
+import './detail.css';
 // import styled from 'styled-components';
 
 // let Box = styled.div`
@@ -14,62 +16,110 @@ import "./detail.css"
 //     padding: 10px;
 // `;
 
-function Detail(props){
-    let {id} = useParams();
+function Detail(props) {
+    let { id } = useParams();
     let img_id = Number(id) + 1;
-    let 찾은상품 = props.shoes.find((x)=>{
+    let 찾은상품 = props.shoes.find((x) => {
         return x.id == id;
     });
-    let [alert, setAlert ]= useState(true);
-    
-    useEffect(()=>{
-        let a = setTimeout(()=>{
-            setAlert(false); 
-        },2000)
-        return ()=>{
+    let [alert, setAlert] = useState(true);
+    let [탭, 탭변경] = useState(0);
+
+    useEffect(() => {
+        let a = setTimeout(() => {
+            setAlert(false);
+        }, 2000);
+        return () => {
             clearTimeout(a);
-        }
+        };
     });
 
     let [isNumber, setIsNumber] = useState(true);
+    let [fade, setFade] = useState('');
 
- return(
-    <div className="container">
-        {
-            alert == true
-            ? <div className="alert alert-warning">
-                2초 이내 구매시 할인    
-            </div>
-            : null
-        }
-            
-        <img src={"https://codingapple1.github.io/shop/shoes"+img_id+".jpg"} width="100%" alt=''/>
-            
-            
-        {
-            isNumber == false
-            ?  <div className='alert alert-danger'>
-                숫자만 입력하시오
-            </div>
-            : null
-        }
-            
-        <input type="text" style = {{width: "200px"}} onChange={(e)=>{
-            if(!isNaN(e.target.value)&& !isNumber){
-                setIsNumber(true);
-            }else if(isNaN(e.target.value) && isNumber){
-                setIsNumber(false);
-            }
-        }}/>
-            
-        <h4 className="pt-5">{찾은상품.title}</h4>
-        <p>{찾은상품.content}</p>
-        <p>{찾은상품.price}원</p>
-        <button className="btn btn-danger">주문하기</button> 
-    
-    </div> 
- )   
+    useEffect(() => {
+        let a = setTimeout(() => {
+            setFade('end');
+        }, 100);
+        return () => {
+            clearTimeout(a);
+            setFade('');
+        };
+    }, [props]);
+
+    return (
+        <div className={'container start ' + fade}>
+            {alert == true ? <div className="alert alert-warning">2초 이내 구매시 할인</div> : null}
+
+            <img src={'https://codingapple1.github.io/shop/shoes' + img_id + '.jpg'} width="100%" alt="" />
+
+            {isNumber == false ? <div className="alert alert-danger">숫자만 입력하시오</div> : null}
+
+            <input
+                type="text"
+                style={{ width: '200px' }}
+                onChange={(e) => {
+                    if (!isNaN(e.target.value) && !isNumber) {
+                        setIsNumber(true);
+                    } else if (isNaN(e.target.value) && isNumber) {
+                        setIsNumber(false);
+                    }
+                }}
+            />
+
+            <h4 className="pt-5">{찾은상품.title}</h4>
+            <p>{찾은상품.content}</p>
+            <p>{찾은상품.price}원</p>
+            <button className="btn btn-danger">주문하기</button>
+            <Nav variant="tabs" defaultActiveKey="link0">
+                <Nav.Item>
+                    <Nav.Link
+                        eventKey="link0"
+                        onClick={() => {
+                            탭변경(0);
+                        }}
+                    >
+                        버튼0
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link
+                        eventKey="link1"
+                        onClick={() => {
+                            탭변경(1);
+                        }}
+                    >
+                        버튼1
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link
+                        eventKey="link2"
+                        onClick={() => {
+                            탭변경(2);
+                        }}
+                    >
+                        버튼2
+                    </Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <TabContent 탭={탭} />
+        </div>
+    );
+}
+
+function TabContent({ 탭 }) {
+    let [fade, setFade] = useState('');
+    useEffect(() => {
+        let a = setTimeout(() => {
+            setFade('end');
+        }, 100);
+        return () => {
+            clearTimeout(a);
+            setFade('');
+        };
+    }, [탭]);
+    return <div className={'start ' + fade}>{[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][탭]}</div>;
 }
 
 export default Detail;
-
